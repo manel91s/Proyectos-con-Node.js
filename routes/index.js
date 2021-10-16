e698d87b6a59245526778fa54347339cb2584e30
Manel-PC <manel.aguilera91@gmail.com>
2021-10-16 09:25:57 +0200

    express y plantillas pug creadas

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const proyectosController = require('../controllers/proyectosController');
+
+
+module.exports = function() {
+    //ruta para el home
+    router.get('/', proyectosController.proyectosHome);
+    router.get('/nosotros', proyectosController.nosotros);
+
+    return router;
+}   
